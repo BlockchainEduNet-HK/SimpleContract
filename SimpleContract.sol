66ec106e63ab111f0b6aec578341251daf9d1b20
Hu Yao-Chieh <yhuag@connect.ust.hk>
2018-03-15 20:07:21 +0800

    init

---
+++ SimpleContract.sol
@@ -0,0 +1,20 @@
+pragma solidity ^0.4.13;
+import "github.com/OpenZeppelin/zeppelin-solidity/contracts/math/SafeMath.sol"; 
+
+contract SimpleContract {
+    using SafeMath for uint;
+    
+    uint256[] public candidate; 
+    
+    function SimpleContract() public {
+        candidate = [0, 0, 0];
+    }
+    
+    function vote(uint8 id) public {
+        candidate[id] = candidate[id].add(1);
+    }
+    
+    function getVote(uint8 id) public view returns (uint256) {
+        return candidate[id];
+    }
+}
